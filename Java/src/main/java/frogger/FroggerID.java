8be2e3c21e955f03ddad556d3fd567f8b2e87c3c
erhes <erheserdenehuu@gmail.com>
2026-04-15 02:09:12 +0800

    finished

--- Java/src/main/java/frogger/FroggerID.java
+++ Java/src/main/java/frogger/FroggerID.java
@@ -1,12 +1,45 @@
 package frogger;
 
-// A record class that can conveniently store immutable data. 
-// To compare two record classes with their corresponding fields, simply use ".equals()".
-// More information: https://docs.oracle.com/en/java/javase/17/language/records.html#GUID-6699E26F-4A9B-4393-A08B-1E47D4B2D263
-public record FroggerID(String firstName,
-                     String lastName,
-                     String phoneNumber,
-                     String zipCode,
-                     String state,
-                     String gender) {
+import java.util.Objects;
+
+public class FroggerID {
+        private String firstName, lastName, phoneNumber, zipCode, state, gender;
+    
+        /**
+         * Constructs a FroggerID with the specified personal information.
+         *
+         * @param firstName the first name
+         * @param lastName the last name
+         * @param phoneNumber the phone number
+         * @param zipCode the zip code
+         * @param state the state
+         * @param gender the gender
+         */
+      public FroggerID(String firstName, String lastName, String phoneNumber,
+                       String zipCode, String state, String gender) {
+            this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.zipCode = zipCode;
+        this.state = state;
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FroggerID)) return false;
+        FroggerID that = (FroggerID) o;
+        return Objects.equals(firstName, that.firstName) &&
+               Objects.equals(lastName, that.lastName) &&
+               Objects.equals(phoneNumber, that.phoneNumber) &&
+               Objects.equals(zipCode, that.zipCode) &&
+               Objects.equals(state, that.state) &&
+               Objects.equals(gender, that.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, phoneNumber, zipCode, state, gender);
+    }
 }
